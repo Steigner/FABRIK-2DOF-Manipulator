19ef459cbc776aee7bb32e7c7721f71860076cf9
Steigner <200543@seznam.cz>
2022-12-07 15:22:43 +0100

    animation

--- Matlab/main.m
+++ Matlab/main.m
@@ -23,6 +23,8 @@ ax_wr = [-140.0, 140.0; -150.0, 150.0];
 
 % choose if you want show workspace / simple animation of trajectory 
 show_workspace = true;
+show_origin_position = true;
+show_anim = true;
 
 % simple workspace show
 if show_workspace
@@ -57,8 +59,6 @@ while 1
     th2 = atan2(p_(3,2) - p_(2,2), p_(3,1) - p_(2,1));
     th2 = - q(1) - th1 + th2;
     
-    q = [pi/2, th1, th2];
-    
     % radians -> degree
     th1_deg = rad2deg(th1);
     th2_deg = rad2deg(th2);
@@ -76,17 +76,7 @@ while 1
     xlabel('x');
     ylabel('y');
     
-    % Color, RGB + Transparency
     h1 = plot( ...
-        p(:,1), ...
-        p(:,2), ...
-        'LineWidth', 3, ...
-        'Color', [0, 0, 0, 0.5], ...
-        'Marker','o', ...
-        'LineStyle','-' ...
-    );
-
-    h2 = plot( ...
         p_(:,1), ...
         p_(:,2), ...
         'LineWidth', 3, ...
@@ -94,7 +84,7 @@ while 1
         'Marker', 'o', ...
         'LineStyle', '-' ...
     );
-    h3 = plot( ...
+    h2 = plot( ...
         goal(1,1), ...
         goal(1,2), ...
         'LineWidth', 5, ...
@@ -102,10 +92,62 @@ while 1
         'Marker', 'x', ...
         'MarkerSize', 10 ...
     );
+
+    legend('Processed position', 'Goal');
+
+    if show_origin_position
+        % Color, RGB + Transparency
+        h3 = plot( ...
+            p(:,1), ...
+            p(:,2), ...
+            'LineWidth', 3, ...
+            'Color', [0, 0, 0, 0.5], ...
+            'Marker','o', ...
+            'LineStyle','-' ...
+        );
+        legend('Processed position', 'Goal', 'Origin position');
+    end  
     
-    legend('Origin position', 'Processed position', 'Goal');
+    if show_anim
+        % simple trajectory generation, defaultly 100 samples
+        te1 = linspace(q(2), th1, 100);
+        te2 = linspace(q(3), th2, 100);
+        % pre-alocate array by zeros
+        % 2 joint
+        p_anim = zeros(length(te1),2);
+        % 3 end effector / tool
+        p2_anim = p_anim;
+        
+        % compute FK x,y points
+        for i=1:length(te1)
+            % call Forward-Kinematics from ./FK function  
+            pom = FK([q(1), te1(i)]);
+            p_anim(i,:) = pom(1:2,4)';
+            pom = FK([q(1), te1(i), te2(i)]);
+            p2_anim(i,:) = pom(1:2,4)';
+        end
+        
+        % animate in for loop by length of generated samples
+        for i=1:length(te1)
+            h_anim = plot( ...
+                [q(1), p_anim(i,1), p2_anim(i,1)], ...
+                [q(1), p_anim(i,2), p2_anim(i,2)], ...
+                'LineWidth', 3, ...
+                'Color', 'blue', ...
+                'Marker', 'o', ...
+                'HandleVisibility', 'off' ...
+            );
+            % pause for 0.05 seconds
+            pause(.05)
+            % delete plot
+            delete(h_anim) % <- comment if you want see all steps comment
+        end
+    end
     
-    % input from plot
+    % change start angles of manipulator
+    q = [q(1), th1, th2];
+
+    % click input from plot
     try
         [x,y,~] = ginput(1); 
     catch
@@ -119,5 +161,7 @@ while 1
     % delete plots for next computation
     delete(h1);
     delete(h2);
-    delete(h3);
+    if show_origin_position
+        delete(h3);
+    end
 end
